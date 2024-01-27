73883d83756cc7733eb446a55a8ae1bc5e7d002b
sebtlm <thelayeredmind@gmail.com>
2024-01-27 18:46:46 +0100

    Add Trouble for diagnostics review

--- lua/tlm/plugins/help.lua
+++ lua/tlm/plugins/help.lua
@@ -4,4 +4,11 @@ return {
   { 'folke/which-key.nvim', opts = {} },
   -- TODO: Add https://github.com/m4xshen/hardtime.nvim
   -- TODO: Add https://github.com/AckslD/nvim-neoclip.lua
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
 }
